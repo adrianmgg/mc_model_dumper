4e2c49bbb99e9255e9b5fee774d3c4e898d7546c
amgg <16143061+adrianmgg@users.noreply.github.com>
2022-01-04 00:45:13 -0800

    basic ModMain, gradle stuff

---
+++ src/main/java/amgg/mc_model_dumper/ModMain.java
@@ -0,0 +1,26 @@
+package amgg.mc_model_dumper;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+    modid = Properties.MODID,
+    name = Properties.NAME,
+    version = Properties.VERSION,
+    acceptableRemoteVersions = "*"
+)
+public class ModMain {
+    private static Logger logger;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
---
+++ src/template/java/amgg/mc_model_dumper/Properties.java
@@ -0,0 +1,8 @@
+package amgg.mc_model_dumper;
+
+class Properties {
+    public static final String MODID = "${modid}";
+    public static final String NAME = "${modname}";
+    public static final String VERSION = "${version}";
+}
+
